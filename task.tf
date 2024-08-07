08ee413de1b57a77c84e0fd73cb71724b0b58a32
akhisarah <akhilkolli1330@gmail.com>
2024-08-07 11:58:22 +0530

    updating the changes

--- task.tf
+++ task.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "ap-southeast-2"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-02346a771f34de8ac"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
